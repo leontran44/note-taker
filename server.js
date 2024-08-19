78c92ecbbd8322654097eaced53b037c5f05864e
Leon <tranhoangquan44@gmail.com>
2024-08-19 20:01:02 +1000

    initiate UUID package and add POST method

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-
+const { v4: uuidv4 } = require('uuid'); // v4 is to generate unique IDs for notes
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -22,9 +22,25 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
+// API Routes
+
+// Reads the `db.json` file and returns all saved notes as JSON
 app.get('/api/notes', (req, res) => {
     fs.readFile(path.join(__dirname, 'db/db.json'), 'utf8', (err, data) => {
     if (err) throw err;
     res.json(JSON.parse(data));
+    });
+});
+
+app.post('/api/notes', (req, res) => {
+    const newNote = { id: uuidv4(), ...req.body }; // Spread operator to add unique ID to the new note
+    fs.readFile(path.join(__dirname, 'db/db.json'), 'utf8', (err, data) => {
+    if (err) throw err;
+    const notes = JSON.parse(data);
+    notes.push(newNote);
+        fs.writeFile(path.join(__dirname, 'db/db.json'), JSON.stringify(notes, null, 2), (err) => {
+        if (err) throw err;
+        res.json(newNote);
+        });
     });
 });
