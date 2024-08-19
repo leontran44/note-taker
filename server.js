f12309bb8f3e17b91420253b045a7204d7413cc7
Leon <tranhoangquan44@gmail.com>
2024-08-19 19:14:52 +1000

    add GET method

--- server.js
+++ server.js
@@ -21,3 +21,10 @@ app.get('/notes', (req, res) => {
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
 });
+
+app.get('/api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname, 'db/db.json'), 'utf8', (err, data) => {
+    if (err) throw err;
+    res.json(JSON.parse(data));
+    });
+});
